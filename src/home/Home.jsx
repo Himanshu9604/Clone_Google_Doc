67fb5d64b8e245acf1e7cc7363dd0d1419e77b07
Himanshu <himanshuyadav201000@gmail.com>
2023-05-04 12:20:18 +0530

    download functionality on textarea

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,18 +1,34 @@
 import React from "react";
+import { HiDownload } from "react-icons/hi";
 import style from "./Home.module.css";
+
 export default function Home() {
- 
+
+  function handleDownloadDoc() {
+    const content = document.getElementById("content").innerHTML;
+    const blob = new Blob([content], { type: "text/pdf" });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "document.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
+  }
+
   return (
     <div>
       <div className={style.main}>
         <div className={style.wrapper}>
-          <div
-           id="printablediv"
-            className={style.textArea}
-            contentEditable="true"
-          />
+          <div id="content" className={style.textArea} contentEditable="true" />
         </div>
       </div>
+      <div onClick={handleDownloadDoc} className={style.downloaddoc}>
+        <HiDownload />
       </div>
+    </div>
   );
 }
+
+
